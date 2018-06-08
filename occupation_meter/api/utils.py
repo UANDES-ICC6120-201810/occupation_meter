54547e8c3b0baab469bb7e77f9f5bf559643ca4d
dbeckdorf <dbeckdorf@miuandes.cl>
2018-06-08 16:56:01 -0400

    go

--- occupation_meter/api/utils.py
+++ occupation_meter/api/utils.py
@@ -1,4 +1,5 @@
 import os
+import requests
 import boto3
 
 from .models import CountRequest
@@ -6,6 +7,9 @@ from .models import CountRequest
 
 
 def download_image(count_request):
+    r = requests.get(
+        url='https://zapo-storage.nyc3.digitaloceanspaces.com/occupation_images/'
+    )
     #TODO: send GET request for image download
     #     https://{bucket}.{endpoint}/{location}/filename}
     pass
